e9723af6121fe1861172a961bc9f918a91e91f4f
jorgescx <j_escobart@hotmail.com>
2024-05-08 08:38:02 -0600

    views and path

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require('express');
+const path = require('path'); 
 const app = express();
 
+app.set('view engine','ejs');
+app.set('views', path.join(__dirname,'views'));
+app.use(express.static(path.join(__dirname,'public')));
+
 const port = 3002;
- app.get("/", (req, res,next) => {
-    console.log(req);
-    res.send("Hello World!");
- })
-
- app.get("/pokemon"),(req,res,next)=> {
+app.get("/", (req, res,next) => {
    console.log(req);
-   res.json({name: "Pikachu", type: "Electric"});
- }
+   res.render('index', {title: "Home"});
+})
 
- app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
- })
+app.get("/pokemon",(req,res,next)=> {
+  res.json({name: "Pikachu", type: "Electric"});
+});
+
+app.listen(port, () => {
+   console.log(`Server is running on port ${port}`);
+})
